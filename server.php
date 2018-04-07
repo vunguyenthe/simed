46e7500956f4bd1ac8de09ed7f7b413a8a917333
Vu Nguyen <vunguyenthe1976@gmail.com>
2018-04-08 04:06:34 +0700

    upd

--- server.php
+++ server.php
@@ -8,7 +8,7 @@ $email    = "";
 $errors = array(); 
 //jdbc:mysql://simed:3306/simed
 // connect to the database
-$db = mysqli_connect('mysql://msqlsimed:3306/', 'user1WW', 'r0IRVgdFo0XoKPAI', 'simed');
+$db = mysqli_connect('jdbc:mysql://msqlsimed:3306/', 'user1WW', 'r0IRVgdFo0XoKPAI', 'simed');
 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
