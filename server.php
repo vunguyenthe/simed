13dbeda56ab24f48995a3606ae7d86144227a19c
Vu Nguyen <vunguyenthe1976@gmail.com>
2018-04-09 11:30:21 +0700

    5 rooms

--- server.php
+++ server.php
@@ -8,7 +8,8 @@ $email    = "";
 $errors = array(); 
 //jdbc:mysql://simed:3306/simed
 // connect to the database
-$db = mysqli_connect('localhost', 'root', '123456', 'simed');
+//$db = mysqli_connect('localhost', 'root', '123456', 'simed');
+$db = mysqli_connect('172.30.99.97', 'user1WW', 'r0IRVgdFo0XoKPAI', 'simed');
 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
